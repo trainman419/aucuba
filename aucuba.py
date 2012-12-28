0a11a18af8e648a38584e26d99865502192f9a4e
Austin Hendrix <ahendrix@willowgarage.com>
2012-12-27 21:43:49 -0800

    Choices working in interpreted mode.

--- aucuba.py
+++ aucuba.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import yaml
+import random
 
 class Block:
    def __init__(self, data):
@@ -18,20 +19,47 @@ class Block:
 
    def run(self, state):
       print "ERROR: Block run() called"
-      print dir(self)
       return self.next
+
+random_stack = []
+choice_stack = []
+
+def run_sequence(seq, state):
+   global random_stack
+   global choice_stack
+   b = seq
+   while b:
+      b = b.run(state)
+      if len(random_stack) > 0 and not isinstance(b, Random) and not isinstance(b, Conditional):
+         b = random.choice(random_stack)
+         random_stack = []
+      elif len(choice_stack) > 0 and not isinstance(b, Choice) and not isinstance(b, Conditional):
+         print
+         # Get input from the user. Prompt them until the give us an answer we
+         # like
+         i = None
+         while i is None:
+            try:
+               i = int(raw_input('> '))
+            except ValueError:
+               i = 0
+            if i < 1 or i > len(choice_stack):
+               print "Please enter a number between 1 and %d"%(len(choice_stack))
+               i = None
+
+         b = choice_stack[i-1] 
+         choice_stack = []
 
 class Sequence(Block):
    def __init__(self, data):
-      self.children = []
+      self.child = None
       Block.__init__(self, data)
       self.name = data['content']
 
    def run(self, state):
-      if len(self.children) > 0:
-         b = self.children[0]
-         while b:
-            b = b.run()
+      if self.child:
+         run_sequence(self.child, state)
+      return self.next
 
 class Link(Block):
    def __init__(self, data):
@@ -42,6 +70,10 @@ class Comment(Block):
    def __init__(self, data):
       Block.__init__(self, data)
       self.comment = data['content']
+
+   def run(self, state):
+      # explicitly do nothing
+      return self.next
 
 class Flag(Block):
    def __init__(self, data):
@@ -61,8 +93,16 @@ class Choice(Block):
       Block.__init__(self, data)
       self.text = data['content']
 
+   def run(self, state):
+      i = len(choice_stack) + 1
+      print "%d) %s"%(i, self.text)
+      choice_stack.append(self.child)
+      return self.next
+
 class Random(Block):
-   pass
+   def run(self, state):
+      random_stack.append(self.child)
+      return self.next
 
 # Expressions
 class Call(Block):
@@ -75,16 +115,28 @@ class Decrement(Block):
       Block.__init__(self, data)
       self.variable = data['variable']
 
+   def run(self, state):
+      state[self.variable] -= 1
+      return self.next
+
 class Increment(Block):
    def __init__(self, data):
       Block.__init__(self, data)
       self.variable = data['variable']
+
+   def run(self, state):
+      state[self.variable] += 1
+      return self.next
 
 class Assignment(Block):
    def __init__(self, data):
       Block.__init__(self, data)
       self.variable = data['variable']
       self.value = data['value']
+
+   def run(self, state):
+      state[self.variable] = self.value
+      return self.next
 
 # Text classes
 class Text(Block):
@@ -94,6 +146,8 @@ class Text(Block):
 
    def run(self, state):
       print self.text
+      print
+      return self.next
 
 class Descriptive(Text):
    pass
@@ -108,9 +162,7 @@ class Speech(Text):
    def __init__(self, data):
       Text.__init__(self, data)
       self.actor = data['actor']
-
-   def run(self, state):
-      print "%s: %s"%(self.actor, self.text)
+      self.text = "%s: %s"%(self.actor, self.text)
 
 
 type_map = {
@@ -146,11 +198,15 @@ def parse_hook(data):
          print "Got unknown type: %s"%(t)
    # Explicit case for top-level object
    elif 'sequences' in data:
-      print "Got toplevel object: ", data
+      # TODO: real object for top-level object
       return data
    elif 'main' in data:
-      print "Got sequences: ", data
-      return data
+      # TODO: real object for top-level sequences
+      out = {}
+      for seq in data:
+         d = { 'type': 'sequence', 'content': seq, 'children': data[seq]}
+         out[seq] = Sequence(d)
+      return out
    else:
       print "Got malformed data: ", data
    print
@@ -180,9 +236,7 @@ def main():
    print
 
    state = {}
-   b = main_data[0]
-   while b:
-      b = b.run(state)
+   run_sequence(main_data, state)
 
 if __name__ == '__main__':
    main()
